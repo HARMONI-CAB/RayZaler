ed1709b95c9e4e7d81e26ba0219708f9ffcf8d83
Gonzalo J. Carracedo Carballal <BatchDrake@gmail.com>
2024-11-29 13:54:21 +0100

    WIP: Add ConicalLens processor and remove Parabolic and Spherical mirror processors

--- LibRZ/include/RayProcessors.h
+++ LibRZ/include/RayProcessors.h
@@ -20,17 +20,16 @@
 #define _RAYPROCESSORS_H
 
 #include "RayProcessors/ApertureStop.h"
+#include "RayProcessors/ConicLens.h"
 #include "RayProcessors/ConicMirror.h"
 #include "RayProcessors/FlatMirror.h"
 #include "RayProcessors/InfiniteMirror.h"
 #include "RayProcessors/LensletArray.h"
 #include "RayProcessors/Obstruction.h"
-#include "RayProcessors/ParabolicMirror.h"
 #include "RayProcessors/PassThrough.h"
 #include "RayProcessors/PhaseScreen.h"
 #include "RayProcessors/RectangularStop.h"
 #include "RayProcessors/SphericalLens.h"
-#include "RayProcessors/SphericalMirror.h"
 #include "RayProcessors/SquareFlatSurface.h"
 
 namespace RZ {
